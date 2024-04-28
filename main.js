71aff9c4f820710fe0b86e90c0693dd48834c3ab
Abraham <abrahameyaife@gmail.com>
2024-04-28 19:16:04 +0100

    finished second task: Store Result

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
 let nums1 = [1, 2, 3, 0, 0, 0];
 let nums2 = [2, 5, 6];
-let m = 3; // Number of elements in nums1
+let m = 3; // Number of elements initially present in nums1
 let n = 3; // Number of elements in nums2
 
 var merge = function(nums1, m, nums2, n) {
-    const mergedArray = [];
-    let i = 0, j = 0;
-    
-    // Iterate over both arrays simultaneously
-    while (i < m && j < n) {
-        if (nums1[i] <= nums2[j]) {
-            mergedArray.push(nums1[i]);
-            i++;
+    let i = m - 1; // Index to track the end of the valid elements in nums1
+    let j = n - 1; // Index to track the end of the valid elements in nums2
+    let k = m + n - 1; // Index to track the end of the merged array in nums1
+
+    // Merge nums1 and nums2 in-place
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
         } else {
-            mergedArray.push(nums2[j]);
-            j++;
+            nums1[k] = nums2[j];
+            j--;
         }
+        k--;
     }
-    
-    // Append remaining elements of nums1, if any
-    while (i < m) {
-        mergedArray.push(nums1[i]);
-        i++;
+
+    // If there are remaining elements in nums2, append them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
     }
-    
-    // Append remaining elements of nums2, if any
-    while (j < n) {
-        mergedArray.push(nums2[j]);
-        j++;
-    }
-    
-    return mergedArray;
 };
 
-// Sort nums1 and nums2 into a single merged array
-let mergedArray = merge(nums1, m, nums2, n);
+// Merge nums1 and nums2 into nums1
+merge(nums1, m, nums2, n);
 
-// Log the sorted merged array
-console.log(mergedArray);
+// Log the updated nums1 array
+console.log(nums1);
